d205acff6b6af92cc1754772c81d1aa7a9ef332a
DanLNguyenn <159000542+DanLNguyenn@users.noreply.github.com>
2025-02-09 23:06:58 -0500

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,10 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  // Comment out these things below to run on local server
+  // output: 'export',
+  // basePath: '/food4u',
+};
+
+export default nextConfig;
